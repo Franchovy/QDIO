5abccae425852a4bf224ddf73c0ff94fa6549217
Maxime Franchot <maxrofra@gmail.com>
2020-05-22 12:51:58 +0900

    Feat: Layout save/load/getList/clear

--- Source/EffectLoader.cpp
+++ Source/EffectLoader.cpp
@@ -10,4 +10,169 @@
 
 #include "EffectLoader.h"
 
-const String EffectLoader::KEYNAME_EFFECT_LIBRARY = "effectlib";
+void EffectLoader::saveEffect(ValueTree &storeEffect) {
+    std::cout << "Saving effect: " << storeEffect.getType().toString() << newLine;
+    std::cout << storeEffect.toXmlString() << newLine;
+
+    ValueTree effectLib("EffectLib");
+
+    auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_LOADED_EFFECTS);
+    if (loadedEffectsData != nullptr) {
+        effectLib = ValueTree::fromXml(*loadedEffectsData);
+    }
+
+    auto effectToOverWrite = effectLib.getChildWithProperty("name",
+                                                            storeEffect.getProperty("name").toString());
+
+    if (effectToOverWrite.isValid()) {
+        std::cout << "Overwriting effect" << newLine;
+        effectLib.removeChild(effectToOverWrite, nullptr);
+    }
+
+    effectLib.appendChild(storeEffect, nullptr);
+
+    auto dataToStore = effectLib.createXml();
+    getAppProperties().getUserSettings()->setValue(KEYNAME_LOADED_EFFECTS, dataToStore.get());
+    getAppProperties().getUserSettings()->save();
+}
+
+ValueTree EffectLoader::loadEffect(String effectName) {
+    ValueTree effectLib("EffectLib");
+
+    auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_LOADED_EFFECTS);
+    if (loadedEffectsData != nullptr) {
+        effectLib = ValueTree::fromXml(*loadedEffectsData);
+        std::cout << "Stored effects:" << newLine << effectLib.toXmlString() << newLine;
+    }
+
+    auto effectToLoad = effectLib.getChildWithProperty("name", effectName);
+    if (effectToLoad.isValid()) {
+        return effectToLoad;
+    } else {
+        return ValueTree();
+    }
+}
+
+StringArray EffectLoader::getEffectsAvailable() {
+    StringArray effectList;
+
+    std::cout << "Effects available: " << newLine;
+
+    ValueTree effectLib("EffectLib");
+    auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_LOADED_EFFECTS);
+    if (loadedEffectsData != nullptr) {
+        effectLib = ValueTree::fromXml(*loadedEffectsData);
+
+        for (int i = 0; i < effectLib.getNumChildren(); i++) {
+            std::cout << effectLib.getChild(i).getProperty("name").toString() << newLine;
+            effectList.add(effectLib.getChild(i).getProperty("name").toString());
+        }
+    }
+
+    return effectList;
+}
+
+void EffectLoader::clearEffect(String effectName) {
+    ValueTree effectLib("EffectLib");
+
+    auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_LOADED_EFFECTS);
+    if (loadedEffectsData != nullptr) {
+        effectLib = ValueTree::fromXml(*loadedEffectsData);
+    }
+
+    auto effectToRemove = effectLib.getChildWithProperty("name", effectName);
+
+    if (effectToRemove.isValid()) {
+        std::cout << "Removing effect" << newLine;
+        effectLib.removeChild(effectToRemove, nullptr);
+    }
+
+    auto dataToStore = effectLib.createXml();
+
+    getAppProperties().getUserSettings()->setValue(KEYNAME_LOADED_EFFECTS, dataToStore.get());
+    getAppProperties().getUserSettings()->save();
+}
+
+void EffectLoader::saveLayout(ValueTree &layout) {
+
+    std::cout << "Saving layout: " << layout.getType().toString() << newLine;
+    std::cout << layout.toXmlString() << newLine;
+
+    ValueTree effectLib("Layouts");
+
+    auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_LOADOUTS);
+    if (loadedEffectsData != nullptr) {
+        effectLib = ValueTree::fromXml(*loadedEffectsData);
+    }
+
+    auto layoutToOverWrite = effectLib.getChildWithProperty("name",
+                                                            layout.getProperty("name").toString());
+
+    if (layoutToOverWrite.isValid()) {
+        std::cout << "Overwriting layout" << newLine;
+        effectLib.removeChild(layoutToOverWrite, nullptr);
+    }
+
+    effectLib.appendChild(layoutToOverWrite, nullptr);
+
+    auto dataToStore = effectLib.createXml();
+    getAppProperties().getUserSettings()->setValue(KEYNAME_LOADOUTS, dataToStore.get());
+    getAppProperties().getUserSettings()->save();
+}
+
+ValueTree EffectLoader::loadLayout(String layoutName) {
+    ValueTree layouts("Layouts");
+
+    auto loadedLayoutsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_LOADOUTS);
+    if (loadedLayoutsData != nullptr) {
+        layouts = ValueTree::fromXml(*loadedLayoutsData);
+        std::cout << "Stored layouts:" << newLine << layouts.toXmlString() << newLine;
+    }
+
+    auto layoutToLoad = layouts.getChildWithProperty("name", layoutName);
+    if (layoutToLoad.isValid()) {
+        return layoutToLoad;
+    } else {
+        return ValueTree();
+    }
+}
+
+StringArray EffectLoader::getLayoutsAvailable() {
+    StringArray layoutsList;
+
+    std::cout << "Effects available: " << newLine;
+
+    ValueTree layouts("Layouts");
+    auto loadedLayoutsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_LOADOUTS);
+    if (loadedLayoutsData != nullptr) {
+        layouts = ValueTree::fromXml(*loadedLayoutsData);
+
+        for (int i = 0; i < layouts.getNumChildren(); i++) {
+            std::cout << layouts.getChild(i).getProperty("name").toString() << newLine;
+            layoutsList.add(layouts.getChild(i).getProperty("name").toString());
+        }
+    }
+
+    return layoutsList;
+}
+
+void EffectLoader::clearLayout(String layoutName) {
+    ValueTree layouts("Layouts");
+
+    auto loadedLayoutsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_LOADOUTS);
+    if (loadedLayoutsData != nullptr) {
+        layouts = ValueTree::fromXml(*loadedLayoutsData);
+    }
+
+    auto layoutToRemove = layouts.getChildWithProperty("name", layoutName);
+
+    if (layoutToRemove.isValid()) {
+        std::cout << "Removing effect" << newLine;
+        layouts.removeChild(layoutToRemove, nullptr);
+    }
+
+    auto dataToStore = layouts.createXml();
+
+    getAppProperties().getUserSettings()->setValue(KEYNAME_LOADOUTS, dataToStore.get());
+    getAppProperties().getUserSettings()->save();
+}
--- Source/EffectLoader.h
+++ Source/EffectLoader.h
@@ -22,92 +22,15 @@ const String KEYNAME_LOADOUTS = "loadouts";
 
 class EffectLoader {
 public:
-    static void saveEffect(ValueTree& storeEffect) {
-        std::cout << "Saving effect: " << storeEffect.getType().toString() << newLine;
-        std::cout << storeEffect.toXmlString() << newLine;
+    // EffectLib
+    static void saveEffect(ValueTree& storeEffect);
+    static ValueTree loadEffect(String effectName);
+    static StringArray getEffectsAvailable();
+    static void clearEffect(String effectName);
 
-        ValueTree effectLib("EffectLib");
-
-        auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_EFFECT_LIBRARY);
-        if (loadedEffectsData != nullptr) {
-            effectLib = ValueTree::fromXml(*loadedEffectsData);
-        }
-
-        auto effectToOverWrite = effectLib.getChildWithProperty("name",
-                storeEffect.getProperty("name").toString());
-
-        if (effectToOverWrite.isValid()) {
-            std::cout << "Overwriting effect" << newLine;
-            effectLib.removeChild(effectToOverWrite, nullptr);
-        }
-
-        effectLib.appendChild(storeEffect, nullptr);
-
-        auto dataToStore = effectLib.createXml();
-
-        getAppProperties().getUserSettings()->setValue(KEYNAME_EFFECT_LIBRARY, dataToStore.get());
-        getAppProperties().getUserSettings()->save();
-    }
-
-    static ValueTree loadEffect(String effectName) {
-        ValueTree effectLib("EffectLib");
-
-        auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_EFFECT_LIBRARY);
-        if (loadedEffectsData != nullptr) {
-            effectLib = ValueTree::fromXml(*loadedEffectsData);
-            std::cout << "Stored effects:" << newLine << effectLib.toXmlString() << newLine;
-        }
-
-        auto effectToLoad = effectLib.getChildWithProperty("name", effectName);
-        if (effectToLoad.isValid()) {
-            return effectToLoad;
-        } else {
-            return ValueTree();
-        }
-    }
-
-    // TODO
-    // static String list getEffects()
-    static StringArray getEffectsAvailable() {
-        StringArray effectList;
-
-        std::cout << "Effects available: " << newLine;
-
-        ValueTree effectLib("EffectLib");
-        auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_EFFECT_LIBRARY);
-        if (loadedEffectsData != nullptr) {
-            effectLib = ValueTree::fromXml(*loadedEffectsData);
-
-            for (int i = 0; i < effectLib.getNumChildren(); i++) {
-                std::cout << effectLib.getChild(i).getProperty("name").toString() << newLine;
-                effectList.add(effectLib.getChild(i).getProperty("name").toString());
-            }
-        }
-
-        return effectList;
-    }
-
-    static void clearEffect(String effectName) {
-        ValueTree effectLib("EffectLib");
-
-        auto loadedEffectsData = getAppProperties().getUserSettings()->getXmlValue(KEYNAME_EFFECT_LIBRARY);
-        if (loadedEffectsData != nullptr) {
-            effectLib = ValueTree::fromXml(*loadedEffectsData);
-        }
-
-        auto effectToRemove = effectLib.getChildWithProperty("name", effectName);
-
-        if (effectToRemove.isValid()) {
-            std::cout << "Removing effect" << newLine;
-            effectLib.removeChild(effectToRemove, nullptr);
-        }
-
-        auto dataToStore = effectLib.createXml();
-
-        getAppProperties().getUserSettings()->setValue(KEYNAME_EFFECT_LIBRARY, dataToStore.get());
-        getAppProperties().getUserSettings()->save();
-    }
-
-private:
-    static const String KEYNAME_EFFECT_LIBRARY;
+    // Layouts
+    static void saveLayout(ValueTree& layout);
+    static ValueTree loadLayout(String layoutName);
+    static StringArray getLayoutsAvailable();
+    static void clearLayout(String layoutName);
 };
