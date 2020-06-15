fdbfef4800cb11fee890fe3d5f683a8801613848
Maxime Franchot <maxrofra@gmail.com>
2020-06-15 09:52:06 +0900

    Ft: robotization/whisperization effect

---
+++ Source/Audio-Effects/RobotizationWhisperization.cpp
@@ -0,0 +1,116 @@
+/*
+  ==============================================================================
+
+    This code is based on the contents of the book: "Audio Effects: Theory,
+    Implementation and Application" by Joshua D. Reiss and Andrew P. McPherson.
+
+    Code by Juan Gil <https://juangil.com/>.
+    Copyright (C) 2017-2019 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#include "RobotizationWhisperization.h"
+
+//==============================================================================
+
+RobotizationWhisperizationAudioProcessor::RobotizationWhisperizationAudioProcessor():
+    BaseEffect()
+    , stft(*this)
+    , paramEffect (new AudioParameterChoice("Effect", "effect", effectItemsUI, effectPassThrough))
+    , paramFftSize (new AudioParameterChoice("FFT size", "fftsize", fftSizeItemsUI, fftSize512))
+                    /*[this](float value){
+                        const ScopedLock sl (lock);
+                        value = (float)(1 << ((int)value + 5));
+                        paramFftSize.setCurrentAndTargetValue (value);
+                        stft.updateParameters((int)paramFftSize.getTargetValue(),
+                                              (int)paramHopSize.getTargetValue(),
+                                              (int)paramWindowType.getTargetValue());
+                        return value;
+                    })*/
+    , paramHopSize (new AudioParameterChoice("Hop size", "hopsize", hopSizeItemsUI, hopSize8))
+                    /*[this](float value){
+                        const ScopedLock sl (lock);
+                        value = (float)(1 << ((int)value + 1));
+                        paramHopSize.setCurrentAndTargetValue (value);
+                        stft.updateParameters((int)paramFftSize.getTargetValue(),
+                                              (int)paramHopSize.getTargetValue(),
+                                              (int)paramWindowType.getTargetValue());
+                        return value;
+                    })*/
+    , paramWindowType (new AudioParameterChoice("Window type", "windowtype", windowTypeItemsUI, STFT::windowTypeHann))
+                       /*[this](float value){
+                           const ScopedLock sl (lock);
+                           paramWindowType.setCurrentAndTargetValue (value);
+                           stft.updateParameters((int)paramFftSize.getTargetValue(),
+                                                 (int)paramHopSize.getTargetValue(),
+                                                 (int)paramWindowType.getTargetValue());
+                           return value;
+                       })*/
+{
+    setLayout(1,1);
+
+    addParameter(paramEffect);
+    addParameter(paramFftSize);
+    addParameter(paramHopSize);
+    addParameter(paramWindowType);
+}
+
+RobotizationWhisperizationAudioProcessor::~RobotizationWhisperizationAudioProcessor()
+{
+}
+
+//==============================================================================
+
+void RobotizationWhisperizationAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    const double smoothTime = 1e-3;
+    /*paramEffect.reset (sampleRate, smoothTime);
+    paramFftSize.reset (sampleRate, smoothTime);
+    paramHopSize.reset (sampleRate, smoothTime);
+    paramWindowType.reset (sampleRate, smoothTime);*/
+
+    //======================================
+
+    stft.setup (getTotalNumInputChannels());
+    stft.updateParameters((int)(float)(1 << ((int)*paramFftSize + 5)),
+                          (int)(float)(1 << ((int)*paramHopSize + 1)),
+                          (int)*paramWindowType);
+}
+
+void RobotizationWhisperizationAudioProcessor::releaseResources()
+{
+}
+
+void RobotizationWhisperizationAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    const ScopedLock sl (lock);
+
+    ScopedNoDenormals noDenormals;
+
+    const int numInputChannels = getTotalNumInputChannels();
+    const int numOutputChannels = getTotalNumOutputChannels();
+    const int numSamples = buffer.getNumSamples();
+
+    //======================================
+
+    stft.processBlock (buffer);
+
+    //======================================
+
+    for (int channel = numInputChannels; channel < numOutputChannels; ++channel)
+        buffer.clear (channel, 0, numSamples);
+}
---
+++ Source/Audio-Effects/RobotizationWhisperization.h
@@ -0,0 +1,182 @@
+/*
+  ==============================================================================
+
+    This code is based on the contents of the book: "Audio Effects: Theory,
+    Implementation and Application" by Joshua D. Reiss and Andrew P. McPherson.
+
+    Code by Juan Gil <https://juangil.com/>.
+    Copyright (C) 2017-2019 Juan Gil.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "../BaseEffects.h"
+#include "STFT.h"
+
+//==============================================================================
+
+class RobotizationWhisperizationAudioProcessor : public BaseEffect
+{
+public:
+    //==============================================================================
+
+    RobotizationWhisperizationAudioProcessor();
+    ~RobotizationWhisperizationAudioProcessor();
+
+    //==============================================================================
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock (AudioSampleBuffer&, MidiBuffer&) override;
+
+    //==============================================================================
+
+
+    StringArray effectItemsUI = {
+        "Pass-through",
+        "Robotization",
+        "Whisperization"
+    };
+
+    enum effectIndex {
+        effectPassThrough = 0,
+        effectRobotization,
+        effectWhisperization,
+    };
+
+    //======================================
+
+    StringArray fftSizeItemsUI = {
+        "32",
+        "64",
+        "128",
+        "256",
+        "512",
+        "1024",
+        "2048",
+        "4096",
+        "8192"
+    };
+
+    enum fftSizeIndex {
+        fftSize32 = 0,
+        fftSize64,
+        fftSize128,
+        fftSize256,
+        fftSize512,
+        fftSize1024,
+        fftSize2048,
+        fftSize4096,
+        fftSize8192,
+    };
+
+    //======================================
+
+    StringArray hopSizeItemsUI = {
+        "1/2 Window",
+        "1/4 Window",
+        "1/8 Window",
+    };
+
+    enum hopSizeIndex {
+        hopSize2 = 0,
+        hopSize4,
+        hopSize8,
+    };
+
+    //======================================
+
+    StringArray windowTypeItemsUI = {
+        "Rectangular",
+        "Bartlett",
+        "Hann",
+        "Hamming",
+    };
+
+    //======================================
+
+    class RobotizationWhisperization : public STFT
+    {
+    public:
+        RobotizationWhisperization (RobotizationWhisperizationAudioProcessor& p) : parent (p)
+        {
+        }
+
+    private:
+        void modification() override
+        {
+            fft->perform (timeDomainBuffer, frequencyDomainBuffer, false);
+
+            auto size = (float)(1 << ((int)fftSize + 5));
+
+            switch ((int)parent.paramEffect->getIndex()) {
+                case effectPassThrough: {
+                    // nothing
+                    break;
+                }
+                case effectRobotization: {
+                    for (int index = 0; index < size; ++index) {
+                        float magnitude = abs (frequencyDomainBuffer[index]);
+                        frequencyDomainBuffer[index].real (magnitude);
+                        frequencyDomainBuffer[index].imag (0.0f);
+                    }
+                    break;
+                }
+                case effectWhisperization: {
+                    for (int index = 0; index < size / 2 + 1; ++index) {
+                        float magnitude = abs (frequencyDomainBuffer[index]);
+                        float phase = 2.0f * M_PI * (float)rand() / (float)RAND_MAX;
+
+                        frequencyDomainBuffer[index].real (magnitude * cosf (phase));
+                        frequencyDomainBuffer[index].imag (magnitude * sinf (phase));
+                        if (index > 0 && index < size / 2) {
+                            frequencyDomainBuffer[size - index].real (magnitude * cosf (phase));
+                            frequencyDomainBuffer[size - index].imag (magnitude * sinf (-phase));
+                        }
+                    }
+                    break;
+                }
+            }
+
+            fft->perform (frequencyDomainBuffer, timeDomainBuffer, true);
+        }
+
+        RobotizationWhisperizationAudioProcessor& parent;
+    };
+
+    //======================================
+
+    CriticalSection lock;
+    RobotizationWhisperization stft;
+
+    //======================================
+
+    AudioParameterChoice* paramEffect;
+    AudioParameterChoice* paramFftSize;
+    AudioParameterChoice* paramHopSize;
+    AudioParameterChoice* paramWindowType;
+
+private:
+    //==============================================================================
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RobotizationWhisperizationAudioProcessor)
+};
