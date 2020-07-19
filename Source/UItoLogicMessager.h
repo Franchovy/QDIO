96243018f2973a3156b58a9d17438225207e2378
Maxime Franchot <maxrofra@gmail.com>
2020-07-19 11:11:58 +0200

    wip: createEffect functions

--- Source/UItoLogicMessager.h
+++ Source/UItoLogicMessager.h
@@ -10,12 +10,20 @@
 
 #pragma once
 
+#include <JuceHeader.h>
+
 class UItoLogicMessager
 {
 public:
     UItoLogicMessager();
+
     static UItoLogicMessager* getInstance();
 
+    // Create Effect functions
+    void createEffect(int processorID); // Processor-based Effect
+    void createEffect(); // Empty Meta-Effect
+    void createEffect(ValueTree data); // Meta-Effect
+    
 
 private:
     static UItoLogicMessager* instance;
